6f71f3751886533cbb272dea90d2a83372c9ed57
1426351320 <1426351320@qq.com>
2019-03-16 15:34:06 +0800

    字符处理

--- 1_test/test.cpp
+++ 1_test/test.cpp
@@ -6,14 +6,53 @@
 #include <string>
 #include <map>
 #include <algorithm>
+#include<unordered_map>
 
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int a(9);
-	float b(3.14);
+	int N(0);
+	vector<string> vecstr;
+	string temp;
+
+	cin >> N;
+	while (N)
+	{
+		cin >> temp;
+		vecstr.push_back(temp);
+		N--;
+	}
+
+
+	for (int i(0); i < vecstr.size(); i++)
+	{
+
+		for (int j(0); j < vecstr[i].size() - 3; j++)
+		{
+			char ch1(vecstr[i][j]), ch2(vecstr[i][j + 1]), ch3(vecstr[i][j + 2]);
+			if (ch2 == ch1 && ch3 == ch2)
+			{
+				vecstr[i].erase(vecstr[i].begin() + j + 2);
+				j--;
+				continue;
+			}
+			if (vecstr[i].size() - j >= 4)
+			{
+				char ch4(vecstr[i][j + 3]);
+				if (ch1 == ch2 && ch3 == ch4)
+				{
+					vecstr[i].erase(vecstr[i].begin() + j + 3);
+					j--;
+					continue;
+				}
+			}
+		}
+	}
+
+	for (int i(0); i < vecstr.size(); i++)
+		cout << vecstr[i] << endl;
 
 	return 0;
 }
